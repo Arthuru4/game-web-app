ad05be2d620cad182e4f0ae68879617a7a519076
Vitalii <vitalii.yermokhin@gmail.com>
2018-10-15 22:56:09 +0300

    architecture project

---
+++ src/client/layouts/Content.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Content extends Component {
+
+    render() {
+        return (
+            <iframe src="../../../src/game/index.html" width="100%" height="100%"> </iframe>
+        )
+    }
+}
