c0ff28c4551df065df49ded3a6447da783d06cc2
ermohin_v <ermohin@river-soft.net>
2018-10-19 17:57:05 +0300

    updated eslint

--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import Header from './layouts/Header';
-import Content from './layouts/Content';
-import Footer from './layouts/Footer';
+import Header from '../layouts/Header';
+import Content from '../layouts/Content';
+import Footer from '../layouts/Footer';
 import store from '../store/index';
-import './styles/app.css';
+import '../styles/app.css';
 
-export default class App extends Component {
+class App extends Component {
     render() {
         return (
             <Provider store={store}>
                 <div>
-                    <Header/>
-                    <Content/>
-                    <Footer/>
+                    <Header />
+                    <Content />
+                    <Footer />
                 </div>
             </Provider>
         );
     }
 }
+
+export default {App};
---
+++ src/client/reducers/topics.js
@@ -0,0 +1,13 @@
+const initialState = {
+    topicsByUrl: {},
+    selectedTopicsUrls: []
+};
+
+function reduce(state = initialState, action = {}) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reduce;
