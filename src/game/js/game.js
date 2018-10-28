b346c3811f72531a60bfe32cc764f39889b15d46
chernobay_a <chernobay_a@river-soft.net>
2018-10-28 15:15:18 +0200

    bugfix/GAMEWEBAPP-000* edit asset-pack load logic #begin

--- src/game/js/game.js
+++ src/game/js/game.js
@@ -3,21 +3,31 @@ import editConfig from '../../../config/config';
 class Game extends Phaser.Scene {
     constructor() {
         super('GameScene');
+        this.gamePatch = `../${editConfig.gamePath.toValue(editConfig.gameName)}/assets/`;
+        // this.assetsChecker = new LoadAssetsManager(this.gamePatch, this.gamePatch);
     }
 
     preload() {
         this.gameConfig = editConfig || null;
 
         if (editConfig.gameName && editConfig.gamePath) {
-            this.load.json('assetPack', `../${this.gameConfig.gamePath.toValue(this.gameConfig.gameName)}/assets/asset-pack.json`, true);
+            this.load.json('assetPack', `${this.gamePatch}asset-pack.json`, true);
+            this.load.path = this.gamePatch;
         }
 
-        this.load.image('img', '../src/game/assets/sprites/sprite.png');
+        this.load.image('img', 'img/sprite.png');
     }
 
     create() {
-        // TODO: HERE NEED TO PARSE ALL JSON KEYS
         this.assetPackJson = this.cache.json.get('assetPack');
+
+        // TODO: load in preload
+        // Object.keys(this.assetPackJson).forEach((arr) => {
+        //     for (let i = 0; i < this.assetPackJson[arr].length; i++) {
+        //         // IF NOT IGNORED IN ASSET-PACK
+        //         this.load.image(this.assetPackJson[arr][i].key, this.assetPackJson[arr][i].url);
+        //     }
+        // });
 
         if (this.gameConfig.gameName === 'tanks') this.add.image(100, 100, 'img');
 
